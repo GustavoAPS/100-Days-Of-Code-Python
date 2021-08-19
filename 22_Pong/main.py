d6cbdbf201eb036fbec1bf2963b86a68aec1c09d
Gustavo Afonso <gugustavo137@gmail.com>
2021-08-18 23:19:54 -0300

    Adding ball class

--- 22_Pong/main.py
+++ 22_Pong/main.py
@@ -1,16 +1,32 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 
 screen = Screen()
+screen.tracer(0)
 screen.setup(width=600, height=400)
 screen.listen()
-right_paddle = Paddle(side="right")
-left_paddle = Paddle(side="left")
 screen.bgcolor("black")
 screen.title("PONG")
+right_paddle = Paddle(side="right")
+left_paddle = Paddle(side="left")
+ball = Ball()
 
 screen.onkey(right_paddle.go_up, "Up")
 screen.onkey(right_paddle.go_down, "Down")
 
+screen.onkey(left_paddle.go_up, "w")
+screen.onkey(left_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    ball.move()
+
+    if ball.ycor() > 190 or ball.ycor() < -190:
+        ball.bounce()
+
+    time.sleep(0.1)
 screen.exitonclick()
--- 22_Pong/paddle.py
+++ 22_Pong/paddle.py
@@ -1,23 +1,23 @@
 from turtle import Turtle
 
 
-class Paddle:
+class Paddle(Turtle):
     def __init__(self, side):
-        self.new_paddle = Turtle()
-        self.new_paddle.penup()
-        self.new_paddle.shape("square")
-        self.new_paddle.color("white")
-        self.new_paddle.shapesize(stretch_wid=5, stretch_len=1)
+        super().__init__()
+        self.penup()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
 
         if side == "right":
-            self.new_paddle.goto(280, 0)
+            self.goto(280, 0)
         if side == "left":
-            self.new_paddle.goto(-280, 0)
+            self.goto(-280, 0)
 
     def go_up(self):
-        new_y = self.new_paddle.ycor() + 20
-        self.new_paddle.goto(self.new_paddle.xcor(), new_y)
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
 
     def go_down(self):
-        new_y = self.new_paddle.ycor() - 20
-        self.new_paddle.goto(self.new_paddle.xcor(), new_y)
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
