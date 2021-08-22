82416144243520d219b29fbd693d74d8faa9474c
Gustavo Afonso <gugustavo137@gmail.com>
2021-08-21 23:39:54 -0300

    Adding data persistance to snake game

--- 21_SnakeGameDay2/scoreboard.py
+++ 21_SnakeGameDay2/scoreboard.py
@@ -8,11 +8,21 @@ class Scoreboard(Turtle):
         super().__init__()
         self.score = 0
         self.high_score = 0
+        self.read_high_score_from_file()
         self.color("white")
         self.penup()
         self.goto(0, 240)
         self.hideturtle()
         self.update_score()
+
+    def read_high_score_from_file(self):
+        with open("data.txt") as file:
+            contents = file.read()
+            self.high_score = int(contents)
+
+    def write_high_score_to_file(self, score):
+        with open("data.txt", mode='w') as file:
+            file.write(str(score))
 
     def update_score(self):
         self.clear()
@@ -22,6 +32,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            self.write_high_score_to_file(self.score)
         self.score = 0
         self.update_score()
 
