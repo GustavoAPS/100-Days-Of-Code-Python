76ef11b9cefdce5264fa871f7eb4b384897cb36c
Gustavo Afonso <gugustavo137@gmail.com>
2021-08-22 01:16:58 -0300

    Adding day 24 finished code

--- 24_MailMerge/main.py
+++ 24_MailMerge/main.py
@@ -1,2 +1,12 @@
-with open("my_file.txt", mode='a') as file:
-    file.write("\nCharmander")
+PLACE_HOLDER = "[name]"
+
+with open("./Input/Names/invited_names.txt", "r") as names_file:
+    names = names_file.readlines()
+
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACE_HOLDER, stripped_name)
+        with open(f"Output/ReadyToSend/letter_for_{stripped_name}", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
