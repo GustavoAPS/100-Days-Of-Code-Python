d9beae3c3c26790b86b25a1baa47bfcc5f8bb0bb
Gustavo Afonso <gugustavo137@gmail.com>
2021-08-31 10:34:14 -0300

    Adding error handle to day 26 project

--- 26_NATO_Alphabet/main.py
+++ 26_NATO_Alphabet/main.py
@@ -1,13 +1,19 @@
 import pandas
 
-
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 alphabet_dict = {}
 for (index, row) in data.iterrows():
     alphabet_dict[row.letter] = row.code
 
-name = (input("What is the word to receive in NATO?")).upper()
+def nato():
+    name = (input("What is the word to receive in NATO?")).upper()
 
-for letters in name:
-    print(alphabet_dict[letters])
+    try:
+        for letters in name:
+            print(alphabet_dict[letters])
+    except KeyError:
+        print("Input must contain only a-z letters")
+        nato()
+
+nato()
