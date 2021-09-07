fab9a8f2fa744bffb9d829a2c1cc9749df7b0740
Gustavo Afonso <gugustavo137@gmail.com>
2021-09-07 01:52:08 -0300

    Changing question base to an api based question base

--- 34_Quiz_With_Api/main.py
+++ 34_Quiz_With_Api/main.py
@@ -3,6 +3,7 @@ from data import question_data
 from quiz_brain import QuizBrain
 
 question_bank = []
+
 for question in question_data:
     question_text = question["question"]
     question_answer = question["correct_answer"]
@@ -11,6 +12,7 @@ for question in question_data:
 
 
 quiz = QuizBrain(question_bank)
+
 
 while quiz.still_has_questions():
     quiz.next_question()
