5ab55c3d8b313a882f3c4367cc00e3fdf96d6736
Gustavo Afonso <gugustavo137@gmail.com>
2021-09-16 22:48:07 -0300

    Adding Flight search class

--- 39_Flight_Deals/flight_search.py
+++ 39_Flight_Deals/flight_search.py
@@ -1,8 +1,18 @@
+import requests
+
+
+TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
+TEQUILA_API_KEY = "Edoqn61yXjQURFofdb1lq9vtVjYG8OT7"
+
+
 class FlightSearch:
-    def __init__(self):
-        pass
 
-    # This class is responsible for talking to the Flight Search API.
+    def get_destination_code(self, city_name):
 
-    def search_flight(self, name):
-        return "TESTING"
+        location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {"term": city_name, "location_types": "city"}
+        response = requests.get(url=location_endpoint, headers=headers, params=query)
+        results = response.json()["locations"]
+        code = results[0]["code"]
+        return code
--- 39_Flight_Deals/main.py
+++ 39_Flight_Deals/main.py
@@ -12,10 +12,10 @@ sheet_data = new_data_manager.get_sheet_data()
 pprint(sheet_data)
 
 
-print(new_fly_search.search_flight("Toronto"))
+print(new_fly_search.get_destination_code("Toronto"))
 
 for line_data in sheet_data:
-    line_data['iataCode'] = new_fly_search.search_flight(line_data['city'])
+    line_data['iataCode'] = new_fly_search.get_destination_code(line_data['city'])
     # new_data_manager.update_line(line_data)
 
 for line_data in sheet_data:
