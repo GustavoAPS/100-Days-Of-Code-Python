9a8a3e9a7fdcd664f094069e1402286f12c96db2
GustavoAPS <gugustavo137@gmail.com>
2021-10-05 22:35:25 -0300

    Adding day 51 twitter bot end code

--- 51_twitter_bot/main.py
+++ 51_twitter_bot/main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+chrome_driver_path = r"Your chrome driver path"
+TWITTER_LOGIN_URL = "https://twitter.com/i/flow/login"
+user_name = "Your twitter login"
+password = "Your twitter password"
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+driver.get(TWITTER_LOGIN_URL)
+
+time.sleep(10)
+username_text_box = driver.find_element_by_name("username")
+username_text_box.send_keys(user_name)
+time.sleep(3)
+username_text_box.send_keys(Keys.ENTER)
+
+time.sleep(5)
+password_text_box = driver.find_element_by_name("password")
+password_text_box.send_keys(password)
+time.sleep(3)
+password_text_box.send_keys(Keys.ENTER)
+
+time.sleep(5)
+tweet_text_box = driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div[2]/div')
+tweet_text_box.send_keys("I live.")
+time.sleep(3)
+tweet_text_box.send_keys(Keys.ENTER)
+time.sleep(2)
+tweet_button = driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[3]/div/div/div[2]/div[3]/div/span/span')
+tweet_button.click()
