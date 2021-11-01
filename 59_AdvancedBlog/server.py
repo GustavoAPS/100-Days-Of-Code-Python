6daa3a4634b1ebe1618e630bdfa03f1fdd2917fc
GustavoAPS <gugustavo137@gmail.com>
2021-10-31 22:31:34 -0300

    59 Adding flask to backend

---
+++ 59_AdvancedBlog/server.py
@@ -0,0 +1,28 @@
+import json
+
+from flask import Flask
+from flask import render_template
+import requests
+
+
+app = Flask(__name__)
+
+
+# def get_posts():
+#    with open("posts/posts_2021.json") as file:
+#        contents = json.load(file)
+#        return contents
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+#@app.route('/blog')
+#def get_blog():
+#    return render_template('blog.html', posts=get_posts())
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
