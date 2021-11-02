d5babd8f88564fdd8dd2d0ebc52acb62651e8cb5
GustavoAPS <gugustavo137@gmail.com>
2021-11-02 11:58:40 -0300

    59 adding javascript function to navbar

--- 59_AdvancedBlog/server.py
+++ 59_AdvancedBlog/server.py
@@ -15,13 +15,24 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/index')
 def home():
     return render_template('index.html')
 
 
-#@app.route('/blog')
-#def get_blog():
-#    return render_template('blog.html', posts=get_posts())
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/post')
+def post():
+    return render_template('post.html')
 
 
 if __name__ == "__main__":
