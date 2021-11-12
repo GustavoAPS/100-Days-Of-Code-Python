63a62f8bf9a58882eac1e7960434e0511a47d4f4
GustavoAPS <gugustavo137@gmail.com>
2021-11-11 22:48:03 -0300

    63 Adding query books in the books database

--- 63_VirtualBookshelf/server.py
+++ 63_VirtualBookshelf/server.py
@@ -7,30 +7,29 @@ cursor = db.cursor()
 
 cursor.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
 
-cursor.execute("INSERT OR REPLACE INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
-cursor.execute("INSERT OR REPLACE INTO books VALUES(2, 'The Hobbit', 'J. R. R. Tolkien', '9.75')")
-cursor.execute("INSERT OR REPLACE INTO books VALUES(3, 'Percy Jackson & the Olympians: The Lightning Thief', 'Rick Riordan', '8.5')")
-
-db.commit()
+# cursor.execute("INSERT OR REPLACE INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+# cursor.execute("INSERT OR REPLACE INTO books VALUES(2, 'The Hobbit', 'J. R. R. Tolkien', '9.75')")
+# cursor.execute("INSERT OR REPLACE INTO books VALUES(3, 'Percy Jackson & the Olympians: The Lightning Thief', 'Rick Riordan', '8.5')")
 
 
-all_books = [
-    {
-        "title": "Harry Potter.1",
-        "author": "J. K. Rowling",
-        "rating": 9,
-    },
-    {
-        "title": "Harry Potter.3",
-        "author": "J. K. Rowling",
-        "rating": 10,
-    },
-    {
-        "title": "A pequena sereia",
-        "author": "Gaby",
-        "rating": 10,
+cursor.execute("SELECT title, author, rating FROM books;")
+dataTuples = cursor.fetchall()
+
+
+all_books = []
+
+for bookTuple in dataTuples:
+    entry = {
+        "title": bookTuple[0],
+        "author": bookTuple[1],
+        "rating": bookTuple[2],
     }
-]
+    all_books.append(entry)
+
+# db.commit()
+
+print(all_books)
+
 
 @app.route('/')
 def home():
