45e6e283d029e377bbe7056155c40d5ee3da6095
GustavoAPS <gugustavo137@gmail.com>
2021-11-11 21:36:46 -0300

    63 Adding books to database

--- 63_VirtualBookshelf/server.py
+++ 63_VirtualBookshelf/server.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
 app = Flask(__name__)
+db = sqlite3.connect("books-collection.db")
+cursor = db.cursor()
+
+cursor.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+
+cursor.execute("INSERT OR REPLACE INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+cursor.execute("INSERT OR REPLACE INTO books VALUES(2, 'The Hobbit', 'J. R. R. Tolkien', '9.75')")
+cursor.execute("INSERT OR REPLACE INTO books VALUES(3, 'Percy Jackson & the Olympians: The Lightning Thief', 'Rick Riordan', '8.5')")
+
+db.commit()
+
 
 all_books = [
     {
