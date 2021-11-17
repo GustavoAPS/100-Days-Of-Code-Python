923aa06670cb47414d28a09a37a16ea5e044c5d6
GustavoAPS <gugustavo137@gmail.com>
2021-11-16 22:42:58 -0300

    66 Adding random cafe api request

---
+++ 66_RestfulAPI/database_manager.py
@@ -0,0 +1,36 @@
+import sqlite3
+import random
+
+
+class DatabaseManager:
+
+    def __init__(self):
+
+        self.all_cafes = []
+        self.db = sqlite3.connect("cafes.db")
+        self.cursor = self.db.cursor()
+        self.cursor.execute("SELECT id, name, map_url, img_url, location, has_sockets, has_toilet, has_wifi, can_take_calls, seats, coffee_price FROM cafe;")
+        self.dataTuples = self.cursor.fetchall()
+        self.get_all_cafes()
+
+    def get_all_cafes(self):
+
+        for self.cafeTuple in self.dataTuples:
+            self.entry = {
+                "id": self.cafeTuple[0],
+                "name": self.cafeTuple[1],
+                "map_url": self.cafeTuple[2],
+                "img_url": self.cafeTuple[3],
+                "location": self.cafeTuple[4],
+                "has_sockets": self.cafeTuple[5],
+                "has_toilet": self.cafeTuple[6],
+                "has_wifi": self.cafeTuple[7],
+                "can_take_calls": self.cafeTuple[8],
+                "seats": self.cafeTuple[9],
+                "coffee_price": self.cafeTuple[10],
+            }
+            self.all_cafes.append(self.entry)
+            # print(self.entry)
+
+    def get_random_cafe(self):
+        return random.choice(self.all_cafes)
--- 66_RestfulAPI/main.py
+++ 66_RestfulAPI/main.py
@@ -1,19 +1,30 @@
 from flask import Flask, jsonify, render_template, request
+import database_manager
 
 
+dataManager = database_manager.DatabaseManager()
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
-    
+
+
+@app.route("/random")
+def get_random_cafe():
+    print(dataManager.get_random_cafe())
+    return render_template("index.html")
+
 
 ## HTTP GET - Read Record
 
+
 ## HTTP POST - Create Record
 
+
 ## HTTP PUT/PATCH - Update Record
+
 
 ## HTTP DELETE - Delete Record
 
