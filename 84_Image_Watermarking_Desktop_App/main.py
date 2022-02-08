afb4360df15bedbca8ab42b365a13912f45fae45
GustavoAPS <gugustavo137@gmail.com>
2022-02-08 09:33:23 -0300

    84 Adding projectk

---
+++ 84_Image_Watermarking_Desktop_App/main.py
@@ -0,0 +1,20 @@
+import numpy as np
+from PIL import Image
+
+# load image
+image = Image.open('Images_to_watermark/lake_in_fall.jpg')
+watermark_stamp = Image.open('watermark_stamps/squares_stamp.png')
+
+# converting image to array
+pix = np.array(image)
+pix_stamp = np.array(watermark_stamp)
+
+# ! this value of 64 ,must be changed according to stamp size
+for m in range(0, 64):
+    for n in range(0, 64):
+        if(pix_stamp[m, n, 0] > 0) or (pix_stamp[m, n, 1] > 0) or (pix_stamp[m, n, 2] > 0):
+            pix[m, n, 0] = pix_stamp[m, n, 0]  # 0 accesses the first channel
+            pix[m, n, 1] = pix_stamp[m, n, 1]  # 1 accesses the second channel
+            pix[m, n, 2] = pix_stamp[m, n, 2]  # 2 accesses the third channel
+
+Image.fromarray(pix).save("ImagesWatermarked/lake_in_fall.jpg")
